0cf7d393e06f08a41a6355f2bb4060a7cea93538
Tuan Tran <tuanntran@hotmail.com>
2025-04-10 07:45:22 -0500

    simplify python code

---
+++ calculation.py
@@ -0,0 +1,11 @@
+# src/get_num_square.py
+import os
+
+
+def get_square(value: int) -> int:
+    return value ** 2
+
+
+if __name__ == "__main__":
+    num = os.environ.get("INPUT_NUM")
+    print(f"result::get_square({num})")
