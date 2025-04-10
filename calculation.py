260a18cffca95fbe3ffbddbf5403eeea194f714c
Tuan Tran <tuanntran@hotmail.com>
2025-04-10 13:48:41 -0500

    add resuable workflow

--- calculation.py
+++ calculation.py
@@ -5,6 +5,12 @@ def get_square(value: int) -> int:
     return value ** 2
 
 
+def get_factorial(value: int) -> int:
+    if value in [0, 1]:
+        return 1
+    return value * get_factorial(value - 1)
+
+
 if __name__ == "__main__":
     num = os.environ.get("INPUT_NUM")
     print(f"result::{get_square(int(num))}")
